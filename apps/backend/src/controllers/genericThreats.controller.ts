e0921bddf5771226564e7adbf804757b96e32399
Valentin Huth <valentin.huth@maibornwolff.de>
2026-04-02 20:14:47 +0200

    WIP: small improvements/comments

--- apps/backend/src/controllers/genericThreats.controller.ts
+++ apps/backend/src/controllers/genericThreats.controller.ts
@@ -5,14 +5,12 @@
 import { NextFunction, Request, Response } from "express";
 import { BadRequestError } from "#errors/bad-request.error.js";
 import { NotFoundError } from "#errors/not-found.error.js";
-import { ChildThreatResponse } from "#types/childThreat.types.js";
 import {
     GenericThreatWithChildCountResponse,
     GenericThreatIdParam,
     GenericThreatResponse,
 } from "#types/genericThreat.types.js";
 import { ProjectIdParam } from "#types/project.types.js";
-import { getChildThreatsByGenericThreatId } from "#services/childThreats.service.js";
 import * as genericThreatsService from "#services/genericThreats.service.js";
 
 /**
@@ -77,36 +75,4 @@ export async function getGenericThreatsWithChildren(
     const genericThreats = await genericThreatsService.getGenericThreatsWithChildCount(projectId);
 
     response.json(genericThreats);
-}
-
-/**
- * Gets all child threats for the specified generic threat.
- *
- * @param {Request} request - The http request.
- * @param {Response} response - The http response.
- * @param {NextFunction} next - The next middleware function.
- */
-export async function getChildThreatsForGenericThreat(
-    request: Request<GenericThreatIdParam, ChildThreatResponse[], void>,
-    response: Response<ChildThreatResponse[]>,
-    next: NextFunction
-): Promise<void> {
-    const projectId = request.params.projectId;
-    const genericThreatId = request.params.genericThreatId;
-
-    const genericThreat = await genericThreatsService.getGenericThreat(genericThreatId);
-
-    if (genericThreat === null) {
-        next(new NotFoundError("Generic threat not found"));
-        return;
-    }
-
-    if (genericThreat.projectId !== projectId) {
-        next(new BadRequestError("Generic threat does not belong to this project"));
-        return;
-    }
-
-    const childThreats = await getChildThreatsByGenericThreatId(genericThreatId);
-
-    response.json(childThreats);
 }
