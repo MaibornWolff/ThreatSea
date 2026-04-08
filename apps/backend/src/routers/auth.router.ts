ceba298ddbf744823cd584269b1a2b844a40c23f
Steffen Gerdes <steffen.gerdes@maibornwolff.de>
2026-04-08 09:18:07 +0200

    fix(auth): add rate limit to login endpoint

--- apps/backend/src/routers/auth.router.ts
+++ apps/backend/src/routers/auth.router.ts
@@ -4,6 +4,7 @@
  */
 
 import express from "express";
+import rateLimit from "express-rate-limit";
 import {
     authenticate,
     authenticationMode,
@@ -14,10 +15,15 @@ import {
 
 export const authRouter = express.Router();
 
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 authentication requests per windowMs
+});
+
 authRouter.get("/authenticationMode", authenticationMode);
 authRouter.get("/status", getAuthStatus);
 
-authRouter.get("/login", authenticate);
+authRouter.get("/login", authLimiter, authenticate);
 
 authRouter.get("/redirect", finalizeAuthentication);
 
