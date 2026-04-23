9ecca51f5523c062c912e8e5bca84f7c52bb7ded
Steffen Gerdes <34650633+SteffenGerdes@users.noreply.github.com>
2026-04-23 11:57:53 +0200

    fix(frontend): fix reload error on subpaths (#616)

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 
 export default defineConfig({
-    base: "./",
+    base: "/",
     plugins: [react(), svgrPlugin()],
     cacheDir: ".vite",
     publicDir: "public",
