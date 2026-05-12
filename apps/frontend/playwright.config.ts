aa2469b3d2056cb0b3be109eaf8fd6b060eeac11
yassine.elayoubi <Yassine.ElAyoubi@partner.bmwgroup.com>
2026-05-12 10:29:40 +0200

    chore: restore playwright config comments

--- apps/frontend/playwright.config.ts
+++ apps/frontend/playwright.config.ts
@@ -14,21 +14,35 @@ process.env["PLAYWRIGHT_FRONTEND_ROOT"] = __dirname;
 const enableAltBrowsers =
     !!process.env["CI"] || process.env["PW_ALL_BROWSERS"] === "1" || process.env["PW_ALL_BROWSERS"] === "true";
 
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
 export default defineConfig({
     timeout: 40000,
     testDir: "./playwright/tests",
+    /* Opt out of parallel tests on CI. */
     fullyParallel: false,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env["CI"],
+    /* Retry on CI only */
     retries: process.env["CI"] ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
     workers: 1,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-        baseURL: "http://localhost:3000",
-        screenshot: "only-on-failure",
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        baseURL: "http://localhost:3000", // Base URL for the application being tested
+        screenshot: "only-on-failure", // Take screenshots only on test failure
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
         video: "on-first-retry",
     },
+    /* Configure projects for major browsers */
     projects: [
+        // Setup project
         {
             name: "setup chromium",
             use: { ...devices["Desktop Chrome"] },
@@ -40,6 +54,7 @@ export default defineConfig({
             name: "chromium",
             use: {
                 ...devices["Desktop Chrome"],
+                // Use prepared auth state.
                 storageState: path.join(__dirname, "tmp/.auth/chromium-user.json"),
             },
             dependencies: ["setup chromium"],
@@ -56,6 +71,7 @@ export default defineConfig({
                       name: "firefox",
                       use: {
                           ...devices["Desktop Firefox"],
+                          // Use prepared auth state.
                           storageState: path.join(__dirname, "tmp/.auth/firefox-user.json"),
                       },
                       dependencies: ["setup firefox"],
@@ -70,6 +86,7 @@ export default defineConfig({
                       name: "webkit",
                       use: {
                           ...devices["Desktop Safari"],
+                          // Use prepared auth state.
                           storageState: path.join(__dirname, "tmp/.auth/webkit-user.json"),
                       },
                       dependencies: ["setup webkit"],
@@ -77,6 +94,7 @@ export default defineConfig({
               ]
             : []),
     ],
+    /* Run your local dev server before starting the tests */
     webServer: {
         command: "npm run dev",
         url: "http://localhost:3000",
