3ac03be40ae739fa30e7d848016a410713b349ec
Steffen Gerdes <34650633+SteffenGerdes@users.noreply.github.com>
2026-09-21 13:59:23 +0000

    chore(ui): render 'about' footer link as hyperlink instead of button (#1236)

--- apps/frontend/playwright/pages/footer-links.page.ts
+++ apps/frontend/playwright/pages/footer-links.page.ts
@@ -5,19 +5,19 @@ export class FooterLinksPage extends BasePage {
     readonly footerVersionLabel: Locator;
     readonly imprintLink: Locator;
     readonly privacyPolicyLink: Locator;
-    readonly aboutThreatSeaButton: Locator;
+    readonly aboutThreatSeaLink: Locator;
     readonly aboutDialogVersionLabel: Locator;
     readonly aboutDialogRepositoryLink: Locator;
     readonly aboutDialogCloseButton: Locator;
 
     constructor(page: Page) {
         super(page);
-        const footerContainer = page.getByTestId("page-footer_version").locator("xpath=ancestor::div[1]");
+        //const footerContainer = page.getByTestId("page-footer_version").locator("xpath=ancestor::div[1]");
 
         this.footerVersionLabel = page.getByTestId("page-footer_version");
-        this.imprintLink = footerContainer.locator('a[href="/imprint"]');
-        this.privacyPolicyLink = footerContainer.locator('a[href="/privacy-policy"]');
-        this.aboutThreatSeaButton = footerContainer.locator('button[type="button"]');
+        this.imprintLink = page.getByTestId("page-footer_imprint_link");
+        this.privacyPolicyLink = page.getByTestId("page-footer_policy_link");
+        this.aboutThreatSeaLink = page.getByTestId("page-footer_about_link");
 
         this.aboutDialogVersionLabel = page.getByTestId("about-dialog_version");
         this.aboutDialogRepositoryLink = page.locator('a[href="https://github.com/MaibornWolff/ThreatSea"]');
